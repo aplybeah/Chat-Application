7a7293c4de9e785548b40f74f478266c3af9c3bf
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-03-28 16:31:38 -0400

    improved landing page; added state to beginning form; attempting to add routes to other components now

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import MainScreen from "./components/MainScreen";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+    this.handleName = this.handleName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleName(event) {
+    this.setState({ name: event.target.value });
+    console.log(this.state.name);
+  }
+  handleSubmit(event) {
+    alert("Nice to meet you " + this.state.name);
+    event.preventDefault();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>What Is Your Name?</h2>
-          <form className="nameCreate">
-            <input type="text" />
+          <form className="nameCreate" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name={this.state.name}
+              onChange={this.handleName}
+            />
           </form>
         </header>
       </div>
--- chat-application/src/components/MainScreen.js
+++ chat-application/src/components/MainScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import NewMessage from "./NewMessage";
+import MessageWindow from "./MessageWindow";
 
 class MainScreen extends Component {
   render() {
     return (
       <div>
+        <MessageWindow />
         <NewMessage />
       </div>
     );
